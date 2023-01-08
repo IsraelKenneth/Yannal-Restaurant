5c2fbbe1f39e55305372ef66171c5cd11ece6818
Israel Kenneth <israel.kenneth2011@gmal.com>
2023-01-08 01:27:24 +0100

    first

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../navbar/navbar.css"
+
+const Navbar = () => {
+  return (
+      <div className='yannal__navbar'>
+          <div className= 'yannal__navbar-links_logo'>
+        Logo
+        </div>
+          <div className='yannal__navbar-links_container'>
+              <p><a href="#home">menu</a></p>
+              <p><a href="#wgpt3">about us</a></p>
+              <p><a href="#possibility">our specials</a></p>
+              <p><a href="#features">our locations</a></p>
+              <p><a href="#blog">our chefs</a></p>
+    </div>
+    <div>
+font awesome
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
